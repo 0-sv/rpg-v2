d1cd57ca083a6ed796113d3872087cba7f046a04
Sebastiaan Verbeek <a.j.s.verbeek@students.uu.nl>
2018-06-02 15:52:51 +0200

    Created boiler for rules

--- STVRogue/GameLogic/Game.cs
+++ STVRogue/GameLogic/Game.cs
@@ -5,44 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using STVRogue.Utils;
 
-namespace STVRogue.GameLogic
-{
-	public class Game
-	{
+namespace STVRogue.GameLogic {
+	public class Game {
 		private Player player;
 		private Dungeon dungeon;
 		
-		/* This creates a player and a random dungeon of the given difficulty level and node-capacity
-         * The player is positioned at the dungeon's starting-node.
-         * The constructor also randomly seeds monster-packs and items into the dungeon. The total
-         * number of monsters are as specified. Monster-packs should be seeded as such that
-         * the nodes' capacity are not violated. Furthermore the seeding of the monsters
-         * and items should meet the balance requirements stated in the Project Document.
-         */
-		public Game(int difficultyLevel, int nodeCapacityMultiplier, int numberOfMonsters)
-		{
-			Logger.log("Creating a game of difficulty level " + difficultyLevel + ", node capacity multiplier "
-					   + nodeCapacityMultiplier + ", and " + numberOfMonsters + " monsters.");
+		public Game(int difficultyLevel, int nodeCapacityMultiplier, int numberOfMonsters) {
 			dungeon = new Dungeon(difficultyLevel, nodeCapacityMultiplier, numberOfMonsters, player);
 		}
 
-		public void Update()
-		{
-			if (player.location.packs.Any())
+		public void Update() {
+			if (player.location.packs.Any()) {
 				player.location.Combat(player);
-			else
-			{
+			}
+			else {
 				Command normalCommand = new Command(player, Console.ReadKey().Key);
 				normalCommand.Execute();
 			}
 		}
 	}
 
-	public class GameCreationException : Exception
-	{
+	public class GameCreationException : Exception {
 		public GameCreationException() { }
-		public GameCreationException(String explanation) : base(explanation)
-		{
+		public GameCreationException(String explanation) : base(explanation) {
 			explanation = "The dungeon is not a valid dungeon!";
 		}
 	}
--- STVRogue/GameLogic/Rules.cs
+++ STVRogue/GameLogic/Rules.cs
@@ -1,12 +1,48 @@
 using System;
+using System.Collections.Generic;
 using STVRogue.GameLogic;
 
 namespace STVRogue.GameLogic {
-    public class Rule {
-        Rule () 
+    public abstract class Rule {
+        public abstract bool IsActive();
     }
 
-    public class RAlert : Rule {
-        
+    public class BaseRule : Rule {
+        protected Dungeon d;
+        public BaseRule (Dungeon d) {
+            this.d = d;
+        }
+
+        public override bool IsActive() {
+            return true;
+        }
+
+        protected List<Node> IsolateZone () {
+            List<Node> result = new List<Node>();
+            // WIP 
+            for (int index = 0; index <= d.Level(d.player.location); index++) {
+                if (ReferenceEquals(d.nodeList[index], new Bridge())) {
+                    index++;
+                }
+
+            }
+            return result;
+        }
+    }
+
+    public class RZone : BaseRule {
+        public RZone(Dungeon d) : base(d) {}
+    }
+
+    public class RNode : BaseRule {
+        public RNode(Dungeon d) : base(d) {}
+    }
+
+    public class RAlert : BaseRule {
+        public RAlert(Dungeon d) : base(d) {}
+    }
+
+    public class REndzone : BaseRule {
+        public REndzone(Dungeon d) : base(d) {}
     }
 }
