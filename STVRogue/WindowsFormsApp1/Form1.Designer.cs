f000beaf81ab6c7e7a6a52c2b3d98d38da7ed2cb
BramT21 <36609057+BramT21@users.noreply.github.com>
2018-06-09 02:24:12 +0200

    UI update

--- STVRogue/WindowsFormsApp1/Form1.Designer.cs
+++ STVRogue/WindowsFormsApp1/Form1.Designer.cs
@@ -38,6 +38,14 @@
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
 			this.label5 = new System.Windows.Forms.Label();
+			this.button6 = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -64,7 +72,6 @@
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(126, 65);
 			this.button3.TabIndex = 2;
-			this.button3.Text = "button3";
 			this.button3.UseVisualStyleBackColor = true;
 			this.button3.Click += new System.EventHandler(this.button3_Click);
 			// 
@@ -74,7 +81,6 @@
 			this.button4.Name = "button4";
 			this.button4.Size = new System.Drawing.Size(126, 65);
 			this.button4.TabIndex = 3;
-			this.button4.Text = "button4";
 			this.button4.UseVisualStyleBackColor = true;
 			this.button4.Click += new System.EventHandler(this.button4_Click);
 			// 
@@ -92,37 +98,39 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("KaiTi", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(419, 181);
+			this.label1.Location = new System.Drawing.Point(408, 334);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(59, 20);
 			this.label1.TabIndex = 6;
 			this.label1.Text = "Items";
+			this.label1.Click += new System.EventHandler(this.label1_Click_1);
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("KaiTi", 11F);
-			this.label2.Location = new System.Drawing.Point(385, 261);
+			this.label2.Location = new System.Drawing.Point(379, 417);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(71, 15);
+			this.label2.Size = new System.Drawing.Size(79, 15);
 			this.label2.TabIndex = 7;
-			this.label2.Text = "Crystals";
+			this.label2.Text = "Crystals:";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
 			// 
 			// label3
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("KaiTi", 11F);
-			this.label3.Location = new System.Drawing.Point(385, 223);
+			this.label3.Location = new System.Drawing.Point(379, 373);
 			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(127, 15);
+			this.label3.Size = new System.Drawing.Size(135, 15);
 			this.label3.TabIndex = 8;
-			this.label3.Text = "Healing Potions";
+			this.label3.Text = "Healing Potions:";
 			// 
 			// label4
 			// 
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("KaiTi", 13F);
-			this.label4.Location = new System.Drawing.Point(27, 236);
+			this.label4.Location = new System.Drawing.Point(12, 416);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(161, 18);
 			this.label4.TabIndex = 9;
@@ -132,17 +140,97 @@
 			// 
 			this.label5.AutoSize = true;
 			this.label5.Font = new System.Drawing.Font("KaiTi", 13F);
-			this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
-			this.label5.Location = new System.Drawing.Point(185, 236);
+			this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.label5.Location = new System.Drawing.Point(173, 416);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(0, 18);
 			this.label5.TabIndex = 10;
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(388, 118);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(126, 65);
+			this.button6.TabIndex = 11;
+			this.button6.Text = "Do nothing";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("KaiTi", 13F);
+			this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.label6.Location = new System.Drawing.Point(512, 373);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(0, 18);
+			this.label6.TabIndex = 12;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("KaiTi", 13F);
+			this.label7.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.label7.Location = new System.Drawing.Point(456, 417);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(0, 18);
+			this.label7.TabIndex = 13;
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(388, 210);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(126, 65);
+			this.button7.TabIndex = 14;
+			this.button7.Text = "Flee from combat";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(185, 201);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(126, 65);
+			this.button8.TabIndex = 15;
+			this.button8.UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(185, 289);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(126, 65);
+			this.button9.TabIndex = 16;
+			this.button9.UseVisualStyleBackColor = true;
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(53, 201);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(126, 65);
+			this.button10.TabIndex = 17;
+			this.button10.UseVisualStyleBackColor = true;
+			// 
+			// button11
+			// 
+			this.button11.Location = new System.Drawing.Point(53, 289);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(126, 65);
+			this.button11.TabIndex = 18;
+			this.button11.UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(561, 312);
+			this.ClientSize = new System.Drawing.Size(549, 467);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.button6);
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
@@ -155,6 +243,7 @@
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "STV-rogue";
+			this.Load += new System.EventHandler(this.Form1_Load);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -172,6 +261,14 @@
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label5;
+		public System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		public System.Windows.Forms.Button button7;
+		public System.Windows.Forms.Button button8;
+		public System.Windows.Forms.Button button9;
+		public System.Windows.Forms.Button button10;
+		public System.Windows.Forms.Button button11;
 	}
 }
 
--- STVRogue/WindowsFormsApp1/Form1.cs
+++ STVRogue/WindowsFormsApp1/Form1.cs
@@ -13,29 +13,20 @@ namespace STVRogue
 {
 	public partial class Form1 : Form
 	{
+		public Game game;
+		public bool inCombat = false;
+		public bool packChosen = false;
+		int packBeingAttacked = 0;
 		public Form1()
 		{
 			InitializeComponent();
 
 
-			Game game = new Game(5, 2, 10);
-			UpdateGame(game);
-
-
-
-
-			//		for (int i = 0; i < game.player.location.neighbors.Count; i++)
-			//		{
-			//		button1.Text = "Go to Node " + game.dungeon.nodeList[i].id;
-			//	}
-			//		while (game.player.HP != 0)
-			//	{
-			//		game.Update();
-
-			//	}
+			game = new Game(5, 2, 10);
+			UpdateGame();
 		}
 
-		public void UpdateGame(Game game)
+		public void UpdateGame()
 		{
 			if (game.player.HP == 0)
 			{
@@ -44,6 +35,9 @@ namespace STVRogue
 			{
 				if (game.player.location.packs.Any())
 				{
+					//		inCombat = true;
+					CombatUI();
+					inCombat = true;
 					Zone z = new Zone(game.dungeon);
 					RAlert alert = new RAlert(z);
 					alert.AlertMonsters();
@@ -54,6 +48,7 @@ namespace STVRogue
 				{
 					UpdateUI(game.player);
 				}
+
 			}
 		}
 		public void UpdateUI(Player player)
@@ -62,59 +57,207 @@ namespace STVRogue
 			button2.Text = "Go to Node " + Int32.Parse(player.location.neighbors[1].id);
 			if (player.location.neighbors.Count > 2)
 			{
+				button3.Show();
 				button3.Text = "Go to Node " + Int32.Parse(player.location.neighbors[2].id);
 			}
 			else
 			{
-				button3.Text = "";
+				button3.Hide();
 			}
 			if (player.location.neighbors.Count > 3)
 			{
+				button4.Show();
 				button4.Text = "Go to Node " + Int32.Parse(player.location.neighbors[3].id);
 			}
 			else
 			{
-				button4.Text = "";
+				button4.Hide();
 			}
 			label5.Text = "Node " + player.location.id;
-		//	label3.Text = ;
-			for(int i = 0; i<player.bag.Count;i++)
+			label6.Text = game.player.bag.OfType<HealingPotion>().Count().ToString();
+			label7.Text = game.player.bag.OfType<Crystal>().Count().ToString();
+			button7.Hide();
+			button8.Hide();
+			button9.Hide();
+			button10.Hide();
+			button11.Hide();
+
+			//	button1.Hide();
+
+		}
+
+		public void CombatUI()
+		{
+			if (!packChosen)
 			{
-			//	if(i is HealingPotion)
+				button1.Text = "Attack pack 1";
+				button2.Hide();
+				if (game.player.location.packs.Count > 1)
+				{
+					button2.Text = "Attack pack 2";
+					button2.Show();
+				}
+				button3.Hide();
+				button4.Hide();
+				button5.Hide();
+				button6.Hide();
+				button7.Hide();
+				button8.Hide();
+				button9.Hide();
+				button10.Hide();
+				button11.Hide();
+
 			}
+			else
+			{
+				button2.Show();
+				button3.Show();
+				button4.Show();
+				button5.Show();
+				button6.Show();
+				button7.Show();
+				button1.Text = "Attack monster 1 (HP " + game.player.location.packs[packBeingAttacked].members[0].HP + "/" + game.player.location.packs[packBeingAttacked].members[0].HPbase + ")";
+				button2.Text = "Attack monster 2 (HP " + game.player.location.packs[packBeingAttacked].members[1].HP + "/" + game.player.location.packs[packBeingAttacked].members[1].HPbase + ")";
+
+			}
+
 
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			string x = button1.Text.Split(' ')[3];
+
+			if (!inCombat)
+			{
+				int x = Int32.Parse(button1.Text.Split(' ')[3]);
+				game.player.location = game.dungeon.nodeList[x];
+				UpdateGame();
+			}
+			else if (!packChosen)
+			{
+
+				packBeingAttacked = 0;
+				packChosen = true;
+				CombatUI();
+			}
+			else
+			{
+				game.player.Attack(game.player.location.packs[packBeingAttacked].members[0]);
+				UpdateGame();
+			}
 
 
 
 		}
 		private void button2_Click(object sender, EventArgs e)
 		{
+			if (!inCombat)
+			{
+				int x = Int32.Parse(button2.Text.Split(' ')[3]);
+				game.player.location = game.dungeon.nodeList[x];
+				UpdateGame();
+			}
+			else if (!packChosen)
+			{
+
+				packBeingAttacked = 1;
+				packChosen = true;
+				CombatUI();
+			}
+			else
+			{
+				game.player.Attack(game.player.location.packs[packBeingAttacked].members[1]);
+				UpdateGame();
+			}
 
 		}
 
 		private void button3_Click(object sender, EventArgs e)
 		{
+			if (!inCombat)
+			{
+				int x = Int32.Parse(button3.Text.Split(' ')[3]);
+				game.player.location = game.dungeon.nodeList[x];
+				UpdateGame();
+			}
+			else if (!packChosen)
+			{
 
+				packBeingAttacked = 2;
+				packChosen = true;
+				CombatUI();
+			}
+			else
+			{
+				game.player.Attack(game.player.location.packs[packBeingAttacked].members[2]);
+				packChosen = false;
+				UpdateGame();
+			}
 		}
 		private void button4_Click(object sender, EventArgs e)
 		{
+			if (!inCombat)
+			{
+				int x = Int32.Parse(button4.Text.Split(' ')[3]);
+				game.player.location = game.dungeon.nodeList[x];
+				UpdateGame();
+			}
+			else if (!packChosen)
+			{
 
+				packBeingAttacked = 3;
+				packChosen = true;
+				CombatUI();
+			}
+			else
+			{
+				game.player.Attack(game.player.location.packs[packBeingAttacked].members[3]);
+				UpdateGame();
+			}
 		}
 
 		private void button5_Click(object sender, EventArgs e)
 		{
-			// player.Heal();
+			if (game.player.bag.OfType<HealingPotion>().Any())
+			{
+				game.player.Heal();
+			}
+			UpdateGame();
 		}
-
+		private void button6_Click(object sender, EventArgs e)
+		{
+			if (inCombat)
+			{
+				if (game.player.bag.OfType<Crystal>().Any())
+				{
+					game.player.Accelerate();
+				}
+			}
+			UpdateGame();
+		}
 		private void label1_Click(object sender, EventArgs e)
 		{
 
 		}
 
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
