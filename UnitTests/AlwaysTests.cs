7cd13f9fd3c2104346b5a80bdbf3663087abe48a
BramT21 <36609057+BramT21@users.noreply.github.com>
2018-06-20 14:09:48 +0200

    testing fixes

--- UnitTests/AlwaysTests.cs
+++ UnitTests/AlwaysTests.cs
@@ -7,15 +7,18 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace UnitTests {
-	public class AlwaysTests {
+namespace UnitTests
+{
+	public class AlwaysTests
+	{
 		Game g;
 		Gamestate gs;
 		Savegame savegame;
 		string data;
 		string path;
 		int[] gameturns;
-		public AlwaysTests() {
+		public AlwaysTests()
+		{
 			gameturns = new int[8];
 			gameturns[0] = 23;
 			gameturns[1] = 32;
@@ -41,9 +44,12 @@ namespace UnitTests {
 			*/
 
 		[Fact]
-		public void test_RZone() {
+		public void test_RZone()
+		{
 			int l, c, m;
-			for (int k = 1; k < 9; k++) {
+			int counter = 0;
+			for (int k = 1; k < 9; k++)
+			{
 				if (k < 5)
 				{
 					l = 5;
@@ -73,21 +79,25 @@ namespace UnitTests {
 				savegame = new Savegame(gs);
 
 				path = @"C:/Users/win7/Documents/GitHub/Software-Testing-Assignment-2/STVRogue/Gameplays/game" + k + "/game" + k + "_turn";
-				for (int i = 0; i < gameturns[k - 1]; i++) {
-					foreach (Pack pack in g.dungeon.packs) {
-						Always always = new Always((G => pack.zone == G.dungeon.CurrentLevel(pack.location)));
-						data = savegame.OpenFile(i, path);
-						Gamestate gamestate = new Gamestate(g, data);
-						Assert.True(always.test(g));
+				for (int i = 0; i < gameturns[k - 1]; i++)
+				{
+					counter = 0;
+					data = savegame.OpenFile(i, path);
+					Gamestate gamestate = new Gamestate(g, data);
+					foreach (Pack pack in gamestate.g.dungeon.packs)
+					{
+						Always always = new Always((G => G.dungeon.packs[counter++].zone == G.dungeon.CurrentLevel(pack.location)));				
+						Assert.True(always.test(gamestate.g));
 
 					}
 				}
 			}
 		}
-		
-        [Fact]
-        public void test_RNode() {
-            int monstersOnNode = 0;
+
+		[Fact]
+		public void test_RNode()
+		{
+			int monstersOnNode = 0;
 			int l, c, m;
 			for (int k = 1; k < 9; k++)
 			{
@@ -119,25 +129,133 @@ namespace UnitTests {
 				gs = new Gamestate(g);
 				savegame = new Savegame(gs);
 				path = @"C:/Users/win7/Documents/GitHub/Software-Testing-Assignment-2/STVRogue/Gameplays/game" + k + "/game" + k + "_turn";
-				for (int i = 0; i < gameturns[k - 1]; i++) {
+				for (int i = 0; i < gameturns[k - 1]; i++)
+				{
+					data = savegame.OpenFile(i, path);
+					Gamestate gamestate = new Gamestate(g, data);
+					foreach (Node node in gamestate.g.dungeon.nodeList)
+					{
+						monstersOnNode = 0;
+						foreach (Pack pack in node.packs)
+						{
+							monstersOnNode += pack.members.Count();
+						}
+						Always always = new Always((G => monstersOnNode <= G.dungeon.multiplier * (G.dungeon.Level(node) + 1)));
+						
+						Assert.True(always.test(gamestate.g));
+					}
+				}
+			}
 
-                    foreach (Node node in g.dungeon.nodeList) {
-                        monstersOnNode = 0;
-                        foreach (Pack pack in node.packs) {
-                            monstersOnNode += pack.members.Count();
-                        }
-                        Always always = new Always((G => monstersOnNode <= G.dungeon.multiplier * (G.dungeon.Level(node) + 1)));
-                        data = savegame.OpenFile(i, path);
-                        Gamestate gamestate = new Gamestate(g, data);
-                        Assert.True(always.test(g));
 
-                    }
-                }
-            }
-			
-	
-        }
-		
+		}
+		[Fact]
+		public void test_REndZone()
+		{
+			int counter = 0;
+			int l, c, m;
+			bool REndZone = false;
+			for (int k = 1; k < 9; k++)
+			{
+				if (k < 5)
+				{
+					l = 5;
+				}
+				else
+				{
+					l = 10;
+				}
+				if (k % 2 == 0)
+				{
+					c = 3;
+				}
+				else
+				{
+					c = 2;
+				}
+				if (k == 3 || k == 4 || k == 7 || k == 8)
+				{
+					m = 50;
+				}
+				else
+				{
+					m = 30;
+				}
+				g = new Game(l, c, m, true);
+				gs = new Gamestate(g);
+				savegame = new Savegame(gs);
+				path = @"C:/Users/win7/Documents/GitHub/Software-Testing-Assignment-2/STVRogue/Gameplays/game" + k + "/game" + k + "_turn";
+				for (int j = 0; j < gameturns[k - 1]-1; j++)
+				{
+					counter = 0;
+					data = savegame.OpenFile(j, path);
+					Gamestate gamestate1 = new Gamestate(g, data);
+					data = savegame.OpenFile(j + 1, path);
+					Gamestate gamestate = new Gamestate(g, data);
+					if (gamestate1.g.dungeon.Level(gamestate1.g.dungeon.player.location) == l)
+					{
+						REndZone = true;
+					}
+					for (int i = gamestate1.g.dungeon.nodeList.Count() - 1; i > gamestate1.g.dungeon.nodeList.Count() - 6; i--)
+					{
+						counter = 0;
+						if (REndZone)
+						{ // check if packs do not move away from the player when the endzone has been reached
+							foreach (Pack pack in gamestate1.g.dungeon.nodeList[i].packs)
+							{								
+								
+								Always always = new Always((G => G.dungeon.Shortestpath(pack.location, G.dungeon.player.location).Count() == G.dungeon.Shortestpath(gamestate.g.dungeon.nodeList[i].packs[counter++].location, G.dungeon.player.location).Count()));
+								Assert.True(always.test(gamestate1.g));
+							}
+						}
+						
+					}
+				}
 
-    }
+			}
+
+		}
+		/*
+		[Fact]
+		public void test_RAlert()
+		{
+			int l, c, m;
+			Node loc;
+			bool REndZone = false;
+			for (int k = 1; k < 9; k++)
+			{
+				if (k < 5)
+				{
+					l = 5;
+				}
+				else
+				{
+					l = 10;
+				}
+				if (k % 2 == 0)
+				{
+					c = 3;
+				}
+				else
+				{
+					c = 2;
+				}
+				if (k == 3 || k == 4 || k == 7 || k == 8)
+				{
+					m = 50;
+				}
+				else
+				{
+					m = 30;
+				}
+				g = new Game(l, c, m, true);
+				gs = new Gamestate(g);
+				savegame = new Savegame(gs);
+				path = @"C:/Users/win7/Documents/GitHub/Software-Testing-Assignment-2/STVRogue/Gameplays/game" + k + "/game" + k + "_turn";
+			}
+			*/
+
+
+
+	}
 }
