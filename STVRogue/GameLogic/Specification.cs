0ae66460919c62e7e690a0bc67aa18d777ffcd5b
Jesse van den Berg <j.s.vandenberg3@students.uu.nl>
2018-06-21 13:12:23 +0200

    Unless class created

--- STVRogue/GameLogic/Specification.cs
+++ STVRogue/GameLogic/Specification.cs
@@ -18,4 +18,12 @@ namespace STVRogue.GameLogic {
 		public Always(Predicate<Game> p) { this.p = p; }
 		public bool test(Game G) { return p(G); }
 	}
+
+    public class Unless : Specification
+    {
+        private Predicate<Game> p1;
+        private Predicate<Game> p2;
+        public Unless(Predicate<Game> p1, Predicate<Game> p2) { this.p1 = p1; this.p2 = p2; }
+        public bool test(Game G) { return p1(G) || p2(G); }
+    }
 }
